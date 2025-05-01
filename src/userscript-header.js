b81c30e0fc242d047289ae2288522fcd28e475e2
LukasHe0908 <52617815+LukasHe0908@users.noreply.github.com>
2025-05-02 05:19:46 +0800

    Up version

--- src/userscript-header.js
+++ src/userscript-header.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name     React Userscripts for read.nlc.cn
 // @namespace https://github.com/siefkenj/crowdmark-addons
-// @version  1.13
+// @version  1.14
 // @description A sample userscript built using react
 // @include *://read.nlc.cn*
 // @run-at   document-start
